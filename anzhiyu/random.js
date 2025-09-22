7a5b3d9e4df2dc38c32d06799b26b986dc126a46
sunf1531 <sunf1531@gmail.com>
2025-09-22 16:20:24 +0800

    Site updated: 2025-09-22 16:20:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/21/hello-world/","2025/09/21/这是一篇新的文章a/"];function toRandomPost(){
+var posts=["2025/09/22/这是另一篇新的博文/","2025/09/21/这是一篇新的文章a/","2025/09/21/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
